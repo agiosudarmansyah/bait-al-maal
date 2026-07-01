d8a8ac6827f4e10844227a9b9fb5e3d9b863bce1
Agio Sudarmansyah <agiosudarmansyah.dev@gmail.com>
2026-07-02 00:13:32 +0700

    Added service layer for feature

--- rust/core/src/account/service.rs
+++ rust/core/src/account/service.rs
@@ -1,8 +1,10 @@
-use crate::error::*;
+use anyhow::{ anyhow, bail,  Result, };
+use uuid::Uuid;
+
 use crate::{
-    Account,
-    AccountType::{self},
-    AccountRepository,
+    Account, 
+    AccountRepository, 
+    AccountType::{self}, 
     TursoAccountRepository,
 };
 
@@ -11,7 +13,31 @@ pub struct AccountService {
 }
 
 impl AccountService {
-    pub async fn create (
+    pub fn new(repository: TursoAccountRepository) -> Self {
+        Self { repository }
+    }
+
+    pub async fn get_by_id(&self, id: Uuid) -> Result<Account> {
+        if id.is_nil() {
+            bail!("Required UUID is missing");
+        }
+
+        let account = self
+            .repository
+            .get_by_id(id)
+            .await?
+            .ok_or_else(|| anyhow!("No account was found"))?;
+
+        Ok(account)
+    }
+
+    pub async fn get_all(&self) -> Result<Vec<Account>> {
+        let account = self.repository.get_all().await?;
+
+        Ok(account)
+    }
+
+    pub async fn create(
         &self,
         name: String,
         icon_key: String,
@@ -26,6 +52,16 @@ impl AccountService {
         );
 
         self.repository.create(&account).await?;
+
+        Ok(())
+    }
+
+    pub async fn delete(&self, id: Uuid) -> Result<()> {
+        if id.is_nil() {
+            bail!("Required UUID is missing")
+        }
+
+        self.repository.delete(id).await?;
 
         Ok(())
     }
--- rust/core/src/error.rs
+++ rust/core/src/error.rs
@@ -10,6 +10,8 @@ pub enum AppError {
     #[error("Uuid parse error")]
     UuidParseError(#[from] uuid::Error),
 
+    #[error("Uuid is missing or no input")]
+    UuidNotFound
 }
 
 pub type Result<T> = std::result::Result<T, AppError>;
