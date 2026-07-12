6fc1dd50202b2faba486370214ffa0e31ec7be25
Agio Sudarmansyah <agiosudarmansyah.dev@gmail.com>
2026-07-12 16:16:17 +0700

    refactor(sqlx): Refactored database using SQLite and sqlx as database manager while subsequently refactoring Account repository

--- rust/core/src/features/account/repository.rs
+++ rust/core/src/features/account/repository.rs
@@ -5,13 +5,14 @@ use uuid::Uuid;
 use crate::account::{
     Account,
 };
-use crate::shared::error::AppError;
+use crate::shared::error::Result;
 
 #[automock]
 #[async_trait]
 pub trait AccountRepository {
-    async fn get_by_id(&self, id: Uuid) -> Result<Option<Account>, AppError>;
-    async fn get_all(&self) -> Result<Vec<Account>, AppError>;
-    async fn create(&self, account: &Account) -> Result<(), AppError>;
-    async fn delete(&self, id: Uuid) -> Result<(), AppError>;
+    async fn find_by_id(&self, id: Uuid) -> Result<Option<Account>>;
+    async fn require_by_id(&self, id: Uuid) -> Result<Account>;
+    async fn require_all(&self) -> Result<Vec<Account>>;
+    async fn create(&self, account: &Account) -> Result<()>;
+    async fn delete(&self, id: Uuid) -> Result<()>;
 }
--- rust/core/src/infrastructure/database/mod.rs
+++ rust/core/src/infrastructure/database/mod.rs
@@ -2,5 +2,5 @@ pub mod connection;
 pub mod mapper;
 pub mod repository;
 
-pub use connection::AppDatabase;
-pub use repository::account_repository::TursoAccountRepository;
+pub use connection::Database;
+pub use repository::account_repository::SqliteAccountRepository;
